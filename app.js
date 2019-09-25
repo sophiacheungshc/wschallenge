018de346dfca1386b8d7489f63ca5acb5ca5dd40
Sophia Cheung <cheungsinhan@gmail.com>
2019-09-25 00:22:08 -0700

    Finish index card styling

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ fetch('http://localhost:3000/data')
         res.groups.forEach((furniture, idx) => {
             const card = document.createElement('div');
             card.setAttribute('class', 'card');
+            card.setAttribute('style', `background-image: url(${furniture.hero.href})`);
             
             const modal = document.createElement('div');
             modal.setAttribute('class', 'modal close');
@@ -21,21 +22,25 @@ fetch('http://localhost:3000/data')
 
             const h1 = document.createElement('h1');
             h1.textContent = furniture.name;
-            const img = document.createElement('img');
-            img.src = furniture.thumbnail.href;
+
+            // priceRange => selling => high/low
+            // price => regular/selling
+            const price = document.createElement('h5');
+            price.textContent = `$ ${furniture.priceRange ? furniture.priceRange.selling.high : furniture.price.regular}`;
             
             container.appendChild(card);
             card.appendChild(modal);
             modal.appendChild(button);
 
             card.appendChild(h1);
-            card.appendChild(img);
+            // card.appendChild(img);
+            card.appendChild(price);
 
-            img.addEventListener('click', function(){
+            card.addEventListener('click', function(){
                 document.getElementById(`${idx}`).classList.toggle("close");
             })
             button.addEventListener('click', function(){
-                document.getElementById(`${idx}`).classList.toggle("close");
+                document.getElementById(`${idx}`).classList.add("close");
             })
         })
     })
